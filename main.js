0829d3b90b4582735460d18117f45ef1f3b0aefe
abhishek.chauhan <abhishek.chauhan@volansys.com>
2024-06-04 12:27:05 +0530

    added modal and fixed the font size

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ window.onresize = function() {
     if(window.innerWidth <= 575) svgImage.classList.add('order-first');
     else svgImage.classList.remove('order-first');
 }
+
+function redirectLetsTalk() {
+    location.replace("https://abhishek0chauhan.github.io/letstalk/lobby.html");
+}
+
+function redirectManagedice() {
+    location.replace("https://managedice.netlify.app/");
+}
