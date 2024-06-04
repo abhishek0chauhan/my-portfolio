210c9d3877e2a4f23d348b85b7334c79cf58e4e5
abhishek.chauhan <abhishek.chauhan@volansys.com>
2024-06-04 18:58:08 +0530

    Added table in the  modal

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let svgImage = document.getElementById('svg-group');
+let getModalBody = document.getElementById('table-project');
 
 window.onresize = function() {
     if(window.innerWidth <= 575) svgImage.classList.add('order-first');
@@ -12,3 +13,32 @@ function redirectLetsTalk() {
 function redirectManagedice() {
     location.replace("https://managedice.netlify.app/");
 }
+
+getModalBody.innerHTML = `
+<tbody>
+                <tr>
+                    <th>My Contribution</th>
+                    <td>
+                        <ul>
+                            <li>Design and development of the REST APIs.</li>
+                            <li>Design and development of the frontend application using Angular.</li>
+                            <li>Modification in portal UI.</li>
+                            <li>I worked on AWS services such as Lambda, DynamoDB, Athena, API Gateway, SES, SNS, SQS, IoT core, MQTT, Cloudwatch, and many more.</li>
+                            <li>I worked on various feature enhancements and bug fixes in the portal and backend.</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Development Tools</th>
+                    <td>Visual Studio Code, Postman</td>
+                </tr>
+                <tr>
+                    <th>Database</th>
+                    <td>DynamoDB</td>
+                </tr>
+                <tr>
+                    <th>Technology</th>
+                    <td>NodeJS, ExpressJS, Angular, AWS</td>
+                </tr>
+            </tbody>
+`
